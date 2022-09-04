52d2ba1b2ce113e48dcff6ee32a902318294064e
jzx <945627077@qq.com>
2022-09-04 17:58:14 +0800

    使用tauri

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    strictPort: true,
     proxy: {
       "/api/v1/": {
         target: "http://127.0.0.1:9081",
@@ -12,4 +13,19 @@ export default defineConfig({
     },
   },
   plugins: [vue()],
+
+  clearScreen: false,
+  // Tauri expects a fixed port, fail if that port is not available
+  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+  // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+  // env variables
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri supports es2021
+    target: ["es2021", "chrome100", "safari13"],
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 });
